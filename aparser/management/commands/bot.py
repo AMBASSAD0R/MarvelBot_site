cc8618359a9c95626587c0a2a26f8df8e457e4c6
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2021-04-18 19:51:37 +0300

    Add functions work database

--- aparser/management/commands/bot.py
+++ aparser/management/commands/bot.py
@@ -21,3 +21,20 @@ def read_json(path):
     with open(path, "r", encoding='utf-8') as read_file:
         data = json.load(read_file)
         return data
+
+
+ def Users_in_base(user_id):
+    try:
+        p = Users.objects.get(user_id=user_id)
+        return True
+    except Users.DoesNotExist:
+        return False
+
+
+def User_add(user_id, type_user='user', col_proj=0):
+    p = Users(
+        user_id=user_id,
+        type_user=type_user,
+        col_proj=col_proj,
+    ).save()
+    
