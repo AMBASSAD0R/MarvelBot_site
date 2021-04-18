369cb68f01b69b62240071d23d614f2722d632ae
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2021-04-18 19:45:10 +0300

    Add imports in bot

---
+++ aparser/management/commands/bot.py
@@ -0,0 +1,8 @@
+import telebot
+import sqlite3
+import datetime
+import time
+import json
+from django.core.management.base import BaseCommand
+from aparser.models import Comics, Users, Films
+import os
