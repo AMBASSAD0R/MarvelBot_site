f8184c96f19cde3ef6173c0b73152f3211ebad03
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2021-04-18 19:48:22 +0300

    Add functions for work json files

--- aparser/management/commands/bot.py
+++ aparser/management/commands/bot.py
@@ -6,3 +6,18 @@ import json
 from django.core.management.base import BaseCommand
 from aparser.models import Comics, Users, Films
 import os
+
+
+def check_v(message1):
+    sp = read_json('C:/Users/zuiko/OneDrive/Desktop/MarvelBot/data_json/comics.json')['url_comics']['Marvel']
+    for i in sp.keys():
+        for j in sp[i].keys():
+            if j == message1:
+                return [i, True]
+    return [False]
+
+ 
+def read_json(path):
+    with open(path, "r", encoding='utf-8') as read_file:
+        data = json.load(read_file)
+        return data
